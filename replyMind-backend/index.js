208df2a31ad0d7021f3e77896f46dd3d3c5f05eb
NIhad Adnan Shah <nihad08adnan@gmail.com>
2024-06-06 11:25:50 +0600

    database connected

--- replyMind-backend/index.js
+++ replyMind-backend/index.js
@@ -2,8 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import bodyParser from "body-parser";
+import connectDB from "./config/db.js";
 
 dotenv.config();
+
+connectDB()
+
 const app = express();
 
 app.use(express.json());
